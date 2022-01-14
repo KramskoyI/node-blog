79b21f79f9ffda9e1d5c9fcf81c50a8039d6209a
KramskoyI <91892171+KramskoyI@users.noreply.github.com>
2022-01-14 13:00:44 +0300

    registering user to MongoDB

--- routes/register.js
+++ routes/register.js
@@ -1,11 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcrypt')
-const fs = require('fs')
-const path = require('path')
-const usersBase = path.join(__dirname, 'db/users.json')
-const usersBuffer = fs.readFileSync('db/users.json')
-const users = JSON.parse(usersBuffer.toString());
 const MongoClient = require("mongodb").MongoClient;
 const url = "mongodb://localhost:27017/";
 const mongoClient = new MongoClient(url);
@@ -21,56 +16,36 @@ router.get('/register', function(req, res, next) {
     
 });
 
-
-
 /* POST REG page. */
 router.post('/register', async (req, res, next) => {
   
     try {
       const hashedPassword = await bcrypt.hash(req.body.password, 10);
-      function getUsers() {
-        try {
-         
-          const usersBuffer = fs.readFileSync('db/users.json');
-          return JSON.parse(usersBuffer.toString());
-        } catch (err) {
-          return [];
-        };
-      };
-  
-      const users = getUsers()
-  
+
       let user = {
-        // id: Date.now().toString(),
         name: req.body.name,
         lastName: req.body.lastName,
         email: req.body.email,
         password: hashedPassword,
       }
-      async function run(user) {
-        try {
-            await mongoClient.connect();
-            const db = mongoClient.db("blog");
-            const collection = db.collection("users");
-            const result = await collection.insertOne(user);
+      mongoClient.connect(function(err, client){
+      
+        const db = client.db("blog");
+        const collection = db.collection("users");
+        collection.insertOne(user, function(err, result){
+              
+            if(err){ 
+                return console.log(err);
+            }
             console.log(result);
-            console.log(user);
-        }catch(err) {
-            console.log(err);
-        } finally {
-            await mongoClient.close();
-        }
-      }
-      run();
-
-      users.push(user)
-      fs.writeFileSync('db/users.json', JSON.stringify(users));
+            client.close();
+        });
+    });
   
       res.redirect('/login')
     } catch {
       res.redirect('/register')
     }
-    
 });
   
 
